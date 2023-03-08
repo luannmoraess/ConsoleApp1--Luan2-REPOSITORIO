00a4f4a8070a8556ee02162e1121bdef4f7ed2c2
luannmoraess <luanmoraes10_@hotmail.com>
2023-03-08 09:42:12 -0300

    Desenvolvi os metodos Get

--- ConsoleApp1- Luan2/ConsoleApp1- Luan2/Produto.cs
+++ ConsoleApp1- Luan2/ConsoleApp1- Luan2/Produto.cs
@@ -8,5 +8,26 @@ namespace ConsoleApp1__Luan2
 {
     internal class Produto
     {
+        private int Id;
+        private string Descricao;
+        private decimal Valor;
+
+        public Produto(int id, string descricao, decimal valor)
+        {
+            Id = id;
+            Descricao = descricao;
+            Valor = valor;
+        }
+        public string getId()
+        {
+            return Descricao;
+        }
+
+        public decimal getValor()
+        {
+            return Valor;
+        }
     }
+   
+   
 }
